7e301b86651b2e46f52cefcf10163eb9e8591d43
tommaso2212 <t.pavani97@gmail.com>
2023-10-21 16:36:56 +0200

    Init clean flutter project

--- flutter/personal/lib/constants/app_color.dart
+++
@@ -1,7 +0,0 @@
-import 'dart:ui';
-
-class AppColor {
-  static const Color primaryColor = Color.fromRGBO(214, 90, 49, 1);
-  static const Color backgroundColor = Color.fromRGBO(34, 40, 49, 1);
-  static const Color sectionColor = Color.fromRGBO(57, 62, 70, 1);
-}
--- flutter/personal/lib/routing/app_route.dart
+++
@@ -1,8 +0,0 @@
-enum AppRoute {
-  login('Login', '/login'),
-  home('Home', '/');
-
-  final String name;
-  final String path;
-  const AppRoute(this.name, this.path);
-}
